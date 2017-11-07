1d6cf47bcbe2f7e3dcf87613c96cda62273a9cdd
fahrettinuygun <fufahrettinuygun@gmail.com>
2017-11-07 19:59:08 +0300

    Friendly Projesi

---
+++ Components/Main.js
@@ -0,0 +1,10 @@
+import {Platform, StyleSheet, Text, View} from 'react-native';
+import React,{Component} from 'react'
+export default class Main extends Component<{}>{
+
+  render(){
+    return(
+//deneme
+    );
+  }
+};
